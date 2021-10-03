9a25ca8cef9ba9d32f878411dab5728f1abf8767
Alvi <alvi.utsab@gmail.com>
2021-10-03 02:54:43 -0700

    summary functionality done

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,13 +1,58 @@
 import React, { useContext } from 'react';
-import { Card } from '@mui/material';
+import { Card, CardContent, CardHeader, CircularProgress } from '@mui/material';
 import WeatherContext from '../contexts/WeatherContext';
 
 export default function Summary(){
     const data = useContext(WeatherContext);
 
+    console.log(data);
+
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    const getWeatherDescription = () => {
+        const description = data && 
+            data.weather && 
+            data.weather[0] && 
+            data.weather[0].description.toString();
+        return  capitalizeFirstLetter(description);
+    }
+
+    const getTempInCelcius = () => {
+        return data && 
+        data.main && 
+        data.main.temp &&
+        (data.main.temp - 273).toFixed(0);
+    }
+
+    const getHighTemparature = () => {
+        return data &&
+        data.main &&
+        data.main.temp_max &&
+        (data.main.temp_max - 273).toFixed(0);
+    }
+
+    const getLowTemparature = () => {
+        return data &&
+        data.main &&
+        data.main.temp_min &&
+        (data.main.temp_min - 273).toFixed(0);
+    }
+
     return (
+        data ? 
         <Card>
-            <p>{data && data.main.temp}</p>
+            <CardHeader 
+                title={data.name}
+            />
+            <CardContent>
+                <p>{getWeatherDescription()}</p>
+                <h2>{`${getTempInCelcius()}\u00b0`}</h2>
+                <p>{`H: ${getHighTemparature()}\u00b0 L: ${getLowTemparature()}\u00b0`}</p>
+            </CardContent>
         </Card>
+        :
+        <CircularProgress />
     )
 }
